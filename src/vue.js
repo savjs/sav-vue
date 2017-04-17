1e12d94daa062c7b1a7679dc7538c6887a2b061d
jetiny <86287344@qq.com>
2017-04-17 16:36:27 +0800

    fxied

--- src/vue.js
+++ src/vue.js
@@ -259,7 +259,7 @@ export function vuePlugin (ctx) {
       vueRender.createRoute(action)
       action.set('vue', async (context) => {
         if (context.accepts) {
-          if ('html' != context.accepts('image', 'json', 'html')) {
+          if ('html' !== context.accepts('image', 'json', 'html')) {
             return
           }
         }
